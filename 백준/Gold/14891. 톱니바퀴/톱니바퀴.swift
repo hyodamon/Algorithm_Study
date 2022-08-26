752c65d39664907799c658a61878673d9bf54969
HYOJUN YANG <hyojjang8685@kakao.com>
2022-08-26 16:25:23 +0900

    [Gold V] Title: 톱니바퀴, Time: 12 ms, Memory: 79520 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.swift
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.swift
@@ -1,7 +1,9 @@
 import Foundation
-typealias initVariabe = (cogwheels: [[Int]], K: Int)
 
-func Input() -> initVariabe { // 초기 변수 입력
+typealias initVariabe = (cogwheels: [[Int]], K: Int) // 초기 변수
+
+/// 초기 변수 입력 함수
+func Input() -> initVariabe {
     var cogwheels: [[Int]] = []
     for _ in 0...3 {
         cogwheels.append(readLine()!.map{ Int(String($0))! })
@@ -11,17 +13,22 @@ func Input() -> initVariabe { // 초기 변수 입력
     return (cogwheels, K)
 }
 
+/// 재귀를 통해 회전을 관리하는 함수
+/// idx : 현재 움직일 톱니바퀴, dir : 방향, state : 움직인 톱니바퀴인지 상태를 확인하는 배열 (true면 이미 움직인 톱니바퀴, false면 아직 안 움직인 톱니바퀴)
 func Rotate(idx: Int, dir: Int, state: [Bool]) {
     var tmpState = state
     tmpState[idx] = true
     
     if idx < 3 && cogwheels[idx][2] != cogwheels[idx + 1][6] && !state[idx + 1] {
-        Rotate(idx: idx + 1, dir: -dir, state: tmpState)
+        // 마지막 톱니바퀴가 아니고(idx가 3보다 작은), 맞물리는 곳의 극이 다르고, 그 톱니바퀴가 움직인 톱니바퀴가 아니면
+        Rotate(idx: idx + 1, dir: -dir, state: tmpState) // 반대 방향(-dir)으로 Rotate
     }
     if idx > 0 && cogwheels[idx][6] != cogwheels[idx - 1][2] && !state[idx - 1] {
-        Rotate(idx: idx - 1, dir: -dir, state: tmpState)
+        // 첫 번째 톱니바퀴가 아니고(idx가 0보다 큰), 맞물리는 곳의 극이 다르고, 그 톱니바퀴가 움직인 톱니바퀴가 아니면
+        Rotate(idx: idx - 1, dir: -dir, state: tmpState) // 반대 방향(-dir)으로 Rotate
     }
     
+    // 방향에 맞게 돌려줌
     if dir == 1 {
         RotateRight(idx: idx)
     } else {
@@ -29,18 +36,23 @@ func Rotate(idx: Int, dir: Int, state: [Bool]) {
     }
 }
 
+
+/// 시계 방향으로 회전하는 함수
 func RotateRight(idx: Int) {
     let tmp = cogwheels[idx].last!
     cogwheels[idx].removeLast()
     cogwheels[idx].insert(tmp, at: 0)
 }
 
+/// 시계 반대 방향으로 회전하는 함수
 func RotateLeft(idx: Int) {
     let tmp = cogwheels[idx].first!
     cogwheels[idx].removeFirst()
     cogwheels[idx].append(tmp)
 }
 
+
+/// 점수를 계산하는 함수
 func getScore() -> Int {
     var score = 0
     for idx in 0...3 {
@@ -53,6 +65,7 @@ func getScore() -> Int {
 
 var (cogwheels, K) = Input()
 
+/// 명령어 입력
 for _ in 0..<K {
     let command = readLine()!.split(separator: " ").map{ Int(String($0))! }
     Rotate(idx: command[0] - 1, dir: command[1], state: [false, false, false, false])
