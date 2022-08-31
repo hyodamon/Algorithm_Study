9f9ea3c9600b55261c097c68426a68fc33eb9202
HYOJUN YANG <hyojjang8685@kakao.com>
2022-08-31 22:46:14 +0900

    [Gold V] Title: 인구 이동, Time: 624 ms, Memory: 79684 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.swift
+++ 백준/Gold/16234. 인구 이동/인구 이동.swift
@@ -1,14 +1,6 @@
-//
-//  main.swift
-//  BOJ
-//
-//  Created by Yang Hyojun on 2022/08/30.
-//
-
 import Foundation
 
-typealias inputVariable = (N: Int, L:Int, R:Int)
-let (dr, dc) = ([-1, 1, 0, 0], [0, 0, 1, -1])
+typealias inputVariable = (N: Int, L:Int, R:Int) // 초기 변수
 
 func Input() -> inputVariable {
     let NLR = readLine()!.split(separator: " ").map{ Int(String($0))! }
@@ -16,10 +8,21 @@ func Input() -> inputVariable {
     return (N, L, R)
 }
 
-func BFS(_ startR: Int, _ startC: Int) -> Int {
+let (N, L, R) = Input()
+let (dr, dc) = ([-1, 1, 0, 0], [0, 0, 1, -1])
+var visited = Array(repeating: Array(repeating: 0, count: N), count: N)
+var A: [[Int]] = []
+for _ in 0..<N {
+    A.append(readLine()!.split(separator: " ").map{ Int(String($0))! })
+}
+
+var answer = 0
+
+
+func BFS(_ startR: Int, _ startC: Int) -> Bool {
     visited[startR][startC] = 1
     var union = [(startR, startC)], queue = [(startR, startC)]
-    var sum = A[startR][startC], cnt = 1
+    var sum = A[startR][startC], cnt = 1 // sum: 연합의 인구 수, cnt: 칸의 개수
 
     while !queue.isEmpty {
         let (r, c) = queue.removeFirst()
@@ -27,9 +30,7 @@ func BFS(_ startR: Int, _ startC: Int) -> Int {
             let nr = r + dr[i], nc = c + dc[i]
 
             if nr < 0 || nc < 0 || nr >= N || nc >= N { continue }
-
             if visited[nr][nc] == 1 { continue }
-
             if abs(A[r][c] - A[nr][nc]) >= L && abs(A[r][c] - A[nr][nc]) <= R  {
                 visited[nr][nc] = 1
                 union.append((nr, nc))
@@ -40,43 +41,39 @@ func BFS(_ startR: Int, _ startC: Int) -> Int {
         }
     }
 
+    if cnt == 1 {
+        return false
+    }
     let avg = Int(floor(Double(sum) / Double(cnt)))
     for (r, c) in union {
         A[r][c] = avg
     }
-
-    return cnt
+    return true
 }
 
-let (N, L, R) = Input()
-let blank = Array(repeating: Array(repeating: 0, count: N), count: N)
-var visited = Array(repeating: Array(repeating: 0, count: N), count: N)
-var A: [[Int]] = []
-for _ in 0..<N {
-    A.append(readLine()!.split(separator: " ").map{ Int(String($0))! })
-}
 
-var answer = 0
-
-while true {
-    var chk = false
-    visited = Array(repeating: Array(repeating: 0, count: N), count: N)
-
-    for r in 0..<N {
-        for c in 0..<N {
-            if (visited[r][c] == 0) {
-                if BFS(r, c) >= 2 {
-                    chk = true
+func Looper() -> Int {
+    var answer = 0
+    while true {
+        var flag = false
+        visited = Array(repeating: Array(repeating: 0, count: N), count: N)
+        for r in 0..<N {
+            for c in 0..<N {
+                if (visited[r][c] == 0) {
+                    if BFS(r, c) {
+                        flag = true
+                    }
                 }
             }
         }
+        if !flag {
+            break
+        }
+        answer += 1
     }
-
-    if !chk {
-        break
-    }
-
-    answer += 1
+    
+    return answer
 }
 
-print(answer)
+
+print(Looper())
