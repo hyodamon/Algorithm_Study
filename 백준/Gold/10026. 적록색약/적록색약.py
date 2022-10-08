938f93a9aae1255f7cff3dbefe7b210df7e79468
HYOJUN YANG <hyojjang8685@kakao.com>
2022-10-08 19:29:56 +0900

    [Gold V] Title: 적록색약, Time: 120 ms, Memory: 33184 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -5,14 +5,11 @@ sys.setrecursionlimit(6000)
 input = sys.stdin.readline
 
 N = int(input())
-picture = []
+normal_picture = []
 weird_picture = []
 
 dr = [0, 0, 1, -1]
 dc = [1, -1, 0, 0]
-
-visited1 = [[0 for _ in range(N)] for _ in range(N)]
-visited2 = [[0 for _ in range(N)] for _ in range(N)]
 
 for _ in range(N) :
     tmp = []
@@ -23,39 +20,32 @@ for _ in range(N) :
             tmp2.append('R')
         else :
             tmp2.append(color)
-    picture.append(tmp)
+    normal_picture.append(tmp)
     weird_picture.append(tmp2)
 
-def normal_dfs(r, c) :
+def dfs(r, c, picture) :
     global flag
     for i in range(4) :
         nr, nc = r + dr[i], c + dc[i]
         if 0 <= nr < N and 0 <= nc < N :
-            if visited1[nr][nc] == 0 and picture[r][c] == picture[nr][nc] :
-                visited1[nr][nc] = flag
-                normal_dfs(nr, nc)
-
-def weird_dfs(r, c) :
-    global flag
-    for i in range(4) :
-        nr, nc = r + dr[i], c + dc[i]
-        if 0 <= nr < N and 0 <= nc < N :
-            if visited2[nr][nc] == 0 and weird_picture[r][c] == weird_picture[nr][nc] :
-                visited2[nr][nc] = flag
-                weird_dfs(nr, nc)
+            if visited[nr][nc] == 0 and picture[r][c] == picture[nr][nc] :
+                visited[nr][nc] = flag
+                dfs(nr, nc, picture)
 
 flag = 1
+visited = [[0 for _ in range(N)] for _ in range(N)]
 for i in range(N) :
     for j in range(N) :
-        if visited1[i][j] == 0 :
-            normal_dfs(i, j)
+        if visited[i][j] == 0 :
+            dfs(i, j, normal_picture)
             flag += 1
 print(flag - 1, end=" ")
 
 flag = 1
+visited = [[0 for _ in range(N)] for _ in range(N)]
 for i in range(N) :
     for j in range(N) :
-        if visited2[i][j] == 0 :
-            weird_dfs(i, j)
+        if visited[i][j] == 0 :
+            dfs(i, j, weird_picture)
             flag += 1
 print(flag - 1)
