4540c8478bf3e9458a1e6dd12810ab06b3e01e6c
HYOJUN YANG <hyojjang8685@kakao.com>
2022-10-15 01:04:30 +0900

    [Gold III] Title: 아기 상어, Time: 180 ms, Memory: 33996 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,7 +1,6 @@
 import sys
 from collections import deque
 import heapq
-sys.setrecursionlimit(1000000)
 
 N = int(sys.stdin.readline())
 MAP = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
@@ -12,10 +11,10 @@ dc = [1, -1, 0, 0]
 size = 2
 cnt = 0
 
-def DFS(st, sr, sc) :
+def BFS(st, sr, sc) :
     global size, cnt
-    visited = [[False for _ in range(N)] for _ in range(N)]
-    visited[sr][sc] = True
+    visited = [[0 for _ in range(N)] for _ in range(N)]
+    visited[sr][sc] = 1
     queue = deque()
     queue.append((st, sr, sc))
     area = []
@@ -25,7 +24,7 @@ def DFS(st, sr, sc) :
         t, r, c = queue.popleft()
         for i in range(4) :
             nt, nr, nc = t + 1, r + dr[i], c + dc[i]
-            if nr < 0 or nr >= N or nc < 0 or nc >= N or visited[nr][nc] :
+            if nr < 0 or nr >= N or nc < 0 or nc >= N or visited[nr][nc] == 1 :
                 continue
             
             if size >= MAP[nr][nc] :
@@ -37,7 +36,7 @@ def DFS(st, sr, sc) :
                     heapq.heappush(area, (nt, nr, nc))
                 else :
                     queue.append((nt, nr, nc))
-                    visited[nr][nc] = True
+                    visited[nr][nc] = 1
 
     if area :
         newT, newR, newC = heapq.heappop(area)
@@ -48,14 +47,14 @@ def DFS(st, sr, sc) :
         if size == cnt :
             size += 1
             cnt = 0
-        DFS(newT, newR, newC)
+        BFS(newT, newR, newC)
     else :
         print(st)
     
 for i in range(N) :
     for j in range(N) :
         if MAP[i][j] == 9 :
-            DFS(0, i, j)
+            BFS(0, i, j)
             break
     else: 
         continue
